259cb92a6d5c9e9813ed839dbf8bf170ce83e628
Gustavo Rosa <gth.rosa@uol.com.br>
2019-05-02 18:05:34 -0300

    Finishing up Datasets section.

--- src/components/TheDataset.vue
+++ src/components/TheDataset.vue
@@ -16,18 +16,82 @@
                     </h6>
                 </div>
             </div>
+            <div class="row text-center mt-5">
+                <div class="col-12 col-sm-6 col-md-6 col-lg-4 mb-4"
+                v-for="dataset in datasets"
+                :key="dataset.id">
+                    <b-card
+                    v-b-modal="dataset.type"
+                    :style="{'background-color': dataset.color}">
+                        <b-card-text>
+                            <font-awesome-icon
+                            :icon="['far', dataset.icon]"/>
+                            {{ dataset.type }}
+                        </b-card-text>
+                    </b-card>
+                    <b-modal
+                    :id="dataset.type"
+                    centered>
+                        <template slot="modal-title">
+                            <font-awesome-icon
+                            :icon="['far', dataset.icon]"/>
+                            {{ dataset.type }}
+                        </template>
+                        <template slot="default">
+                            <div
+                            class="text-left mb-2"
+                            v-for="item in dataset.items"
+                            :key="item.id">
+                                <a :href="item.url">
+                                    {{ item.name }}: {{ item.samples }} samples, {{ item.labels }} labels, {{ item.features }} features
+                                </a>
+                            </div>
+                        </template>
+                        <template slot="modal-footer">
+                            <div></div>
+                        </template>
+                    </b-modal>
+                </div>
+            </div>
         </div>
     </section>
 </template>
 
 <script>
+import { datasets } from '@/configs/dataset'
+
 export default {
-    name: 'TheDataset'
+    name: 'TheDataset',
+    data () {
+        return {
+            datasets: datasets
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 #dataset {
     padding: 150px 0
+}
+
+a {
+    color: $black-color;
+    transition: $transition;
+    &:hover {
+        color: $primary-color;
+        text-decoration: none;
+    }
+}
+
+.card {
+    background-color: #eaeaea;
+    color: $white-color;
+    opacity: $opacity;
+    transition: $transition;
+    &:hover {
+        cursor: pointer;
+        opacity: 1;
+    }
 }
 </style>
---
+++ src/configs/dataset.js
@@ -0,0 +1,191 @@
+export const datasets = [
+    {
+        type: 'Biometric Recognition',
+        icon: 'fingerprint',
+        color: '#370878',
+        items: [
+            {
+                name: 'SignRec',
+                samples: '260',
+                labels: '10',
+                features: '?',
+                url: 'http://recogna.tech/files/signrec.rar'
+            }
+        ]
+    },
+    {
+        type: 'Feature Selection',
+        icon: 'lightbulb',
+        color: '#082d78',
+        items: [
+            {
+                name: 'Biological',
+                samples: '?',
+                labels: '?',
+                features: '?',
+                url: 'http://recogna.tech/files/fs-biological.tar.gz'
+            },
+            {
+                name: 'Faces',
+                samples: '?',
+                labels: '?',
+                features: '?',
+                url: 'http://recogna.tech/files/fs-face.tar.gz'
+            },
+            {
+                name: 'Handwritten',
+                samples: '?',
+                labels: '?',
+                features: '?',
+                url: 'http://recogna.tech/files/fs-handwritten.tar.gz'
+            },
+            {
+                name: 'Other',
+                samples: '?',
+                labels: '?',
+                features: '?',
+                url: 'http://recogna.tech/files/fs-other.tar.gz'
+            },
+            {
+                name: 'Texts',
+                samples: '?',
+                labels: '?',
+                features: '?',
+                url: 'http://recogna.tech/files/fs-text.tar.gz'
+            }
+        ]
+    },
+    {
+        type: 'Image Classification',
+        icon: 'image',
+        color: '#087877',
+        items: [
+            {
+                name: 'Caltech101',
+                samples: '8671',
+                labels: '101',
+                features: '784',
+                url: 'http://recogna.tech/files/caltech101.rar'
+            },
+            {
+                name: 'CIFAR-10',
+                samples: '60000',
+                labels: '10',
+                features: '1024',
+                url: 'http://recogna.tech/files/cifar10.rar'
+            },
+            {
+                name: 'HandPD',
+                samples: '?',
+                labels: '?',
+                features: '?',
+                url: 'http://www2.fc.unesp.br/~papa/pub/datasets/Handpd'
+            },
+            {
+                name: 'MNIST',
+                samples: '70000',
+                labels: '10',
+                features: '784',
+                url: 'http://recogna.tech/files/mnist.rar'
+            },
+            {
+                name: 'MPEG-7',
+                samples: '1400',
+                labels: '70',
+                features: '1024',
+                url: 'http://recogna.tech/files/mpeg7.rar'
+            },
+            {
+                name: 'Semeion',
+                samples: '1593',
+                labels: '10',
+                features: '256',
+                url: 'http://recogna.tech/files/semeion.rar'
+            },
+            {
+                name: 'USPS',
+                samples: '9298',
+                labels: '10',
+                features: '784',
+                url: 'http://recogna.tech/files/usps.rar'
+            },
+        ]
+    },
+    {
+        type: 'Network Detection',
+        icon: 'wifi',
+        color: '#08782a',
+        items: [
+            {
+                name: 'uneSPY',
+                samples: '1.01m',
+                labels: '2',
+                features: '23',
+                url: 'http://recogna.tech/files/unespy.rar'
+            },
+            {
+                name: 'Spambase',
+                samples: '4601',
+                labels: '2',
+                features: '48',
+                url: 'http://recogna.tech/files/spambase.rar'
+            }
+        ]
+    },
+    {
+        type: 'Remote Sensing',
+        icon: 'map-marker-alt',
+        color: '#780808',
+        items: [
+            {
+                name: 'Alos-Palsar',
+                samples: '1',
+                labels: '4',
+                features: '?',
+                url: 'http://recogna.tech/files/alos-palsar.rar'
+            },
+            {
+                name: 'CBERS-2B',
+                samples: '1',
+                labels: '6',
+                features: '?',
+                url: 'http://recogna.tech/files/cbers.rar'
+            },
+            {
+                name: 'Furnas',
+                samples: '1',
+                labels: '4',
+                features: '?',
+                url: 'http://recogna.tech/files/furnas.rar'
+            },
+            {
+                name: 'Geoeye',
+                samples: '1',
+                labels: '9',
+                features: '?',
+                url: 'http://recogna.tech/files/geoeye.rar'
+            },
+            {
+                name: 'Ikonos-2 MS',
+                samples: '1',
+                labels: '9',
+                features: '?',
+                url: 'http://recogna.tech/files/ikonos.rar'
+            },
+            {
+                name: 'Landsat 5 TM',
+                samples: '1',
+                labels: '6',
+                features: '?',
+                url: 'http://recogna.tech/files/landsat.rar'
+            },
+            {
+                name: 'Tietê',
+                samples: '1',
+                labels: '4',
+                features: '?',
+                url: 'http://recogna.tech/files/tiete.rar'
+            }
+        ]
+    }
+]
