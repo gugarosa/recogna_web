f7926616806e66595fe24301344aa1a19fd4708c
Gustavo Rosa <gth.rosa@uol.com.br>
2020-06-09 16:24:08 -0300

    Updating datasets URL.

--- src/configs/dataset.js
+++ src/configs/dataset.js
@@ -9,52 +9,52 @@ export const datasets = [
                 samples: '260',
                 labels: '10',
                 features: '?',
-                url: 'http://recogna.tech/files/signrec.rar'
+                url: 'http://recogna.tech/files/datasets/signrec.rar'
             }
         ]
     },
-    {
-        type: 'Feature Selection',
-        icon: 'lightbulb',
-        color: '#382ee1',
-        items: [
-            {
-                name: 'Biological',
-                samples: '?',
-                labels: '?',
-                features: '?',
-                url: 'http://recogna.tech/files/fs-biological.tar.gz'
-            },
-            {
-                name: 'Faces',
-                samples: '?',
-                labels: '?',
-                features: '?',
-                url: 'http://recogna.tech/files/fs-face.tar.gz'
-            },
-            {
-                name: 'Handwritten',
-                samples: '?',
-                labels: '?',
-                features: '?',
-                url: 'http://recogna.tech/files/fs-handwritten.tar.gz'
-            },
-            {
-                name: 'Other',
-                samples: '?',
-                labels: '?',
-                features: '?',
-                url: 'http://recogna.tech/files/fs-other.tar.gz'
-            },
-            {
-                name: 'Texts',
-                samples: '?',
-                labels: '?',
-                features: '?',
-                url: 'http://recogna.tech/files/fs-text.tar.gz'
-            }
-        ]
-    },
+    // {
+    //     type: 'Feature Selection',
+    //     icon: 'lightbulb',
+    //     color: '#382ee1',
+    //     items: [
+    //         {
+    //             name: 'Biological',
+    //             samples: '?',
+    //             labels: '?',
+    //             features: '?',
+    //             url: 'http://recogna.tech/files/datasets/fs_biological.tar.gz'
+    //         },
+    //         {
+    //             name: 'Faces',
+    //             samples: '?',
+    //             labels: '?',
+    //             features: '?',
+    //             url: 'http://recogna.tech/files/datasets/fs_face.tar.gz'
+    //         },
+    //         {
+    //             name: 'Handwritten',
+    //             samples: '?',
+    //             labels: '?',
+    //             features: '?',
+    //             url: 'http://recogna.tech/files/datasets/fs_handwritten.tar.gz'
+    //         },
+    //         {
+    //             name: 'Other',
+    //             samples: '?',
+    //             labels: '?',
+    //             features: '?',
+    //             url: 'http://recogna.tech/files/datasets/fs_other.tar.gz'
+    //         },
+    //         {
+    //             name: 'Texts',
+    //             samples: '?',
+    //             labels: '?',
+    //             features: '?',
+    //             url: 'http://recogna.tech/files/datasets/fs_text.tar.gz'
+    //         }
+    //     ]
+    // },
     {
         type: 'Image Classification',
         icon: 'image',
@@ -65,14 +65,14 @@ export const datasets = [
                 samples: '8671',
                 labels: '101',
                 features: '784',
-                url: 'http://recogna.tech/files/caltech101.rar'
+                url: 'http://recogna.tech/files/datasets/caltech101.rar'
             },
             {
                 name: 'CIFAR-10',
                 samples: '60000',
                 labels: '10',
                 features: '1024',
-                url: 'http://recogna.tech/files/cifar10.rar'
+                url: 'http://recogna.tech/files/datasets/cifar10.rar'
             },
             {
                 name: 'HandPD',
@@ -86,28 +86,28 @@ export const datasets = [
                 samples: '70000',
                 labels: '10',
                 features: '784',
-                url: 'http://recogna.tech/files/mnist.rar'
+                url: 'http://recogna.tech/files/datasets/mnist.rar'
             },
             {
                 name: 'MPEG-7',
                 samples: '1400',
                 labels: '70',
                 features: '1024',
-                url: 'http://recogna.tech/files/mpeg7.rar'
+                url: 'http://recogna.tech/files/datasets/mpeg7.rar'
             },
             {
                 name: 'Semeion',
                 samples: '1593',
                 labels: '10',
                 features: '256',
-                url: 'http://recogna.tech/files/semeion.rar'
+                url: 'http://recogna.tech/files/datasets/semeion.rar'
             },
             {
                 name: 'USPS',
                 samples: '9298',
                 labels: '10',
                 features: '784',
-                url: 'http://recogna.tech/files/usps.rar'
+                url: 'http://recogna.tech/files/datasets/usps.rar'
             },
         ]
     },
@@ -121,35 +121,35 @@ export const datasets = [
                 samples: '2.8m',
                 labels: '15',
                 features: '78',
-                url: 'http://recogna.tech/files/cicids2017.rar'
+                url: 'http://recogna.tech/files/datasets/cicids2017.tar.gz'
             },
             {
                 name: 'ISCXTor2016',
                 samples: '67828',
                 labels: '2',
                 features: '28',
-                url: 'http://recogna.tech/files/tor-nontor.rar'
+                url: 'http://recogna.tech/files/datasets/tor-nontor.tar.gz'
             },
             {
                 name: 'NSL-KDD',
                 samples: '148517',
                 labels: '2',
                 features: '40',
-                url: 'http://recogna.tech/files/nsl-kdd.rar'
+                url: 'http://recogna.tech/files/datasets/nsl-kdd.tar.gz'
             },
             {
                 name: 'uneSPY',
                 samples: '1.01m',
                 labels: '2',
                 features: '23',
-                url: 'http://recogna.tech/files/unespy.rar'
+                url: 'http://recogna.tech/files/datasets/unespy.rar'
             },
             {
                 name: 'Spambase',
                 samples: '4601',
                 labels: '2',
                 features: '48',
-                url: 'http://recogna.tech/files/spambase.rar'
+                url: 'http://recogna.tech/files/datasets/spambase.rar'
             }
         ]
     },
@@ -163,49 +163,49 @@ export const datasets = [
                 samples: '1',
                 labels: '4',
                 features: '?',
-                url: 'http://recogna.tech/files/alos-palsar.rar'
+                url: 'http://recogna.tech/files/datasets/alos-palsar.rar'
             },
             {
                 name: 'CBERS-2B',
                 samples: '1',
                 labels: '6',
                 features: '?',
-                url: 'http://recogna.tech/files/cbers.rar'
+                url: 'http://recogna.tech/files/datasets/cbers.rar'
             },
             {
                 name: 'Furnas',
                 samples: '1',
                 labels: '4',
                 features: '?',
-                url: 'http://recogna.tech/files/furnas.rar'
+                url: 'http://recogna.tech/files/datasets/furnas.rar'
             },
             {
                 name: 'Geoeye',
                 samples: '1',
                 labels: '9',
                 features: '?',
-                url: 'http://recogna.tech/files/geoeye.rar'
+                url: 'http://recogna.tech/files/datasets/geoeye.rar'
             },
             {
                 name: 'Ikonos-2 MS',
                 samples: '1',
                 labels: '9',
                 features: '?',
-                url: 'http://recogna.tech/files/ikonos.rar'
+                url: 'http://recogna.tech/files/datasets/ikonos.rar'
             },
             {
                 name: 'Landsat 5 TM',
                 samples: '1',
                 labels: '6',
                 features: '?',
-                url: 'http://recogna.tech/files/landsat.rar'
+                url: 'http://recogna.tech/files/datasets/landsat.rar'
             },
             {
                 name: 'Tietê',
                 samples: '1',
                 labels: '4',
                 features: '?',
-                url: 'http://recogna.tech/files/tiete.rar'
+                url: 'http://recogna.tech/files/datasets/tiete.rar'
             }
         ]
     }
